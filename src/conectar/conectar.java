9fa36bb76ebe6d23bd3ff2391bcc1700664dd9d4
Martin-Fuentes <martinfuentes012@gmail.com>
2022-07-08 14:40:24 -0500

    Arreglo

--- src/conectar/conectar.java
+++ src/conectar/conectar.java
@@ -19,7 +19,7 @@ public class conectar {
     public Connection getConexion(){
         Connection con = null;
         try{
-           
+           Class.forName("com.mysql.cj.jdbc.Driver");
             con = (Connection) DriverManager.getConnection(URL, USER, CLAVE);
         }catch(Exception e){
             System.out.println("Error: " + e.getMessage());
