a0f9da34910b862d02ee6f48f88bdbd1595a452c
Martin-Fuentes <martinfuentes012@gmail.com>
2022-07-13 16:04:33 -0500

    Reparación de interfaces

--- src/Menu_Estudiante.java
+++ src/Menu_Estudiante.java
@@ -160,7 +160,7 @@ String Usuario;
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                
+                new Menu_Estudiante("").setVisible(true);
             }
         });
     }
